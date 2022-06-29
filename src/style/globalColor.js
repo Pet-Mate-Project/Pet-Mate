6e489acdf1510429cb61fa5660dd35438439394b
heejin <khj16355@naver.com>
2022-06-30 02:43:42 +0900

    :sparkles: Feat : Input 컴포넌트 구현
    Input 컨포넌트 구현 및 globalColor palette 세팅

---
+++ src/style/globalColor.js
@@ -0,0 +1,16 @@
+
+export const palette = {
+  main: "#1D57C1",
+  sub: "#7EACFF",
+  textPoint: "#002D7D",
+  textRedPoint: "#FF2F7A",
+  textDark: "#404040",
+  white: "#FFFFFF",
+  lightGray: "#404040"
+};
+
+const theme = {
+  palette,
+};
+
+export default theme;
