3582c9fb71578a7a2db6bc2d9486ba6676a1019d
heejin <khj16355@naver.com>
2022-06-30 15:30:22 +0900

    :fire: Del : 글로벌 스타일 중복코드 삭제
    Input파일과 Profile파일에 있는 글로벌 스타일 중복 코드 삭제

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import GlobalStyles from '../../style/globalStyle'
 import { SetProfile, Wraper } from './profileStyle'
 import iconProfile from "../../assets/basic-profile.svg"
 
 export function Profile() {
   return (
     <>
-      <GlobalStyles />
       <Wraper>
         <SetProfile
           src={iconProfile} alt='basic-profile' />
