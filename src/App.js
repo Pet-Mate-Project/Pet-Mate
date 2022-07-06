6ccc73a88a7f60a405711709db33c3eb2f3e06fa
minju moon <mmj9908@gmail.com>
2022-07-06 15:51:18 +0900

    :sparkles: Add : 페이지별라우팅
    App.js에서 페이지 라우팅 , index.js에서 라우팅을 위한 작업을 하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import GlobalStyles from './style/globalStyle';
-
-
+import SetLogin from './pages/SetLogin';
+import {Route,Router,Routes} from 'react-router-dom'
+import TempPage from './pages/TempPage';
+import Main from './pages/main/Main';
+import InitPage from './template/InitPage';
+import Login from './pages/login/Login'
+import {AnimatePresence} from 'framer-motion';
+import SignUp from './pages/signUp/SignUp';
+import SignUpMainPage from './pages/signUp/SignUpMain';
 function App() {
   return (
     <>
       <GlobalStyles />
+      <AnimatePresence/>
+      <Routes>
+        <Route path='/' element={<InitPage/>}></Route>
+        <Route path='/main' element={<Main/>}> </Route>
+        <Route path = '/login' element={<Login/>}></Route>
+        <Route path= '/join' element={<SignUpMainPage/>}> </Route>
+        {/* 임시페이지입니다  */}
+        <Route path='/temppage' element={<TempPage/>}></Route>
+
+  
+
+      </Routes>
+      <AnimatePresence/>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(
+  <BrowserRouter>
+    <App tab="home" />
+  </BrowserRouter>);
