23908959dbecee941e4b40b92402f8fee5539488
minju moon <mmj9908@gmail.com>
2022-07-07 09:40:49 +0900

    :sparkles: Add : 누락된라우팅추가
    1. 로그인페이지 밑 이메일로회원가입 버튼

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import GlobalStyles from './style/globalStyle';
+import {Route,Router,Routes} from 'react-router-dom'
+import TempPage from './pages/TempPage';
+import Main from './pages/main/Main';
+import InitPage from './template/InitPage';
+import Login from './pages/login/Login'
+import {AnimatePresence} from 'framer-motion';
+import SignUpMainPage from './pages/signUp/SignUpMain';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <AnimatePresence/>
+      <Routes>
+        <Route path='/' element={<InitPage/>}></Route>
+        {/* 굳이 URL을 바꿀 필요가 없을것 같음. */}
+        {/* <Route path='/main' element={<Main/>}> </Route> */}
+        <Route path = '/login' element={<Login/>}></Route>
+        <Route path= '/join' element={<SignUpMainPage/>}> </Route>
+        {/* 임시페이지입니다  */}
+        <Route path='/temppage' element={<TempPage/>}></Route>
+      </Routes>
+      <AnimatePresence/>
+    </>
+  )
+}
+export default App;
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { LogoStyle, Gap } from './mainStyle.js';
 import { MainStyle } from '../../style/commonLoginStyle'
 import { Link } from "react-router-dom";
-import Login from '../login/Login.jsx';
+
 
 function Main({loading,visible}) {
  
