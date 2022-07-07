fda91d00066cda2012a32d05e5d9d2579929aa92
minju moon <mmj9908@gmail.com>
2022-07-07 23:14:32 +0900

    :adhesive_bandage: Style : 단순코드제거&수정
    test할때 사용했던 margin값 삭제 ,border style 수정

--- src/components/navBack/navBackStyle.js
+++ src/components/navBack/navBackStyle.js
@@ -2,9 +2,7 @@ import styled, { css } from 'styled-components';
 
 
 export const NavWrapper = styled.nav`
-//임시스타일! border-bottom으로사용하세요
-margin:50px; //삭제예정
-border:1px solid #DBDBDB; 
+border-bottom:1px solid #DBDBDB; 
 box-sizing:border-box;
 width: 390px;
 height: 48px;
