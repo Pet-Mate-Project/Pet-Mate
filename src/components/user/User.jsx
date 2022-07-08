68238f489c01a641593b2ae439502cfa6a0a43d7
heejin <khj16355@naver.com>
2022-07-08 15:58:50 +0900

    :lipstick: Add : SnSPost 컴포넌트 구현
    SnS페이지에서 사용되는 Post 컴포넌트 구현, 반응형으로 제작

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FollowToggleBtn } from '../button/Button.jsx'
-import { ProfileIconM } from '../profileIcon/ProfileIcon.jsx'
+import { ProfileIconS } from '../profileIcon/ProfileIcon.jsx'
 import { TextWrapper, UserId, UserName, Wrapper } from './userStyle.js'
 
 
 export function User() {
   return (
     <Wrapper>
-      <ProfileIconM />
+      <ProfileIconS />
       <TextWrapper>
         <UserName>코랑이</UserName>
         <UserId>@coding_arang</UserId>
---
+++ src/template/snsPost/SnsPost.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { User } from '../../components/user/User.jsx'
+import { IconWrap, PostImg, PostText, WrapperArticle, WrapSection, IconImg, DateText } from './snsPostStyle'
+import image from '../../assets/basic-profile.svg'
+import heartIcon from '../../assets/icon-heart.svg'
+import messageIcon from '../../assets/icon-message.svg'
+
+function SnsPost() {
+  return (
+    <article>
+      <WrapperArticle>
+        <User />
+        <WrapSection>
+          <PostText>우리 아랑이가 드디어 코딩을 할 줄 알아요..!! 우리 애기 천재💗🐿️</PostText>
+          <PostImg src={image} />
+          <IconWrap>
+            <IconImg src={heartIcon} />38
+            <IconImg src={messageIcon} />55
+          </IconWrap>
+          <DateText>2022년 07월 01일</DateText>
+        </WrapSection>
+
+      </WrapperArticle>
+
+    </article>
+  )
+}
+
+export default SnsPost
