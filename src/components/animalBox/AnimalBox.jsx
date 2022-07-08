ee566609f7c71aafb6bd3fe33f7d0aebac2d97c0
minju moon <mmj9908@gmail.com>
2022-07-08 14:25:34 +0900

    :lipstick: Style : animalBox컴포넌트
    기존 Product부분 컴포넌트 입니다.

---
+++ src/components/animalBox/AnimalBox.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {AnimalWrapper, TitleTxt,TimeTxt,Img} from './animalStyle'
+import rang from '../../assets/rang.jpg' //나중에 삭제
+
+//사용법
+// <AnimalBox src={} title={"아랑이랑 산책하실 분"} time={"6월 27일 오후 8시"}/>
+export function AnimalBox(props){
+  return(
+    <AnimalWrapper>
+      <Img src={rang}/>
+      <TitleTxt>{props.title}</TitleTxt>
+      <TimeTxt>{props.time}</TimeTxt>
+    </AnimalWrapper>
+  )
+}
