c63775c7842a16e1802791661e34a630eeae7904
minju moon <mmj9908@gmail.com>
2022-07-09 18:25:32 +0900

    :recycle: Style : styled-component 단순화,컴포넌트리팩토링
    길어진 style코드를 분리, 공통 컴포넌트는 재사용가능하도록 MiddleBtn 으로 분리

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,40 +1,16 @@
 import React, { useState } from 'react';
-import { Button } from './buttonStyle';
+import { Button,IconButton } from './buttonStyle';
 import iconEmail from '../../assets/icon-email.svg';
 import iconSign from '../../assets/icon-signup.svg';
 
-export function NextBtn({ nextClick, disabled, message }) {
+//검증관련 버튼 
+export function NextBtn({ nextClick , disabled, message }) {
   if (message === '이미 가입된 이메일 주소 입니다.' || message === '' || !disabled) {
     return <Button type='button'>다음</Button>;
   }
   else {
     return <Button type='button' onClick={nextClick} disabled={!disabled} hover>다음</Button>;
   }
-}
-
-
-export function LoginBtn({ onClick }) {
-  return (
-    <Button onClick={onClick} hover>
-      로그인
-    </Button>
-  );
-}
-
-export function EmailLoginBtn() {
-  return (
-    <Button icon={iconEmail} hover>
-      이메일로 로그인
-    </Button>
-  );
-}
-
-export function JoinBtn() {
-  return (
-    <Button icon={iconSign} hover>
-      회원가입
-    </Button>
-  );
 }
 
 export function StartBtn({ signUp, disabled, message }) {
@@ -47,6 +23,35 @@ export function StartBtn({ signUp, disabled, message }) {
   }
 }
 
+// -------------------------------------------------------
+// 기본 버튼 -> 텍스트 넣어서 사용
+export function LoginBtn({ onClick }) {
+  return (
+    <Button onClick={onClick} hover>
+      로그인
+    </Button>
+  );
+}
+    
+// -------------------------------------------------------
+//아이콘있는 버튼
+export function EmailLoginBtn() {
+  return (
+    <IconButton icon={iconEmail} hover>
+      이메일로 로그인
+    </IconButton>
+  );
+}
+
+export function JoinBtn() {
+  return (
+    <IconButton icon={iconSign} hover>
+      회원가입
+    </IconButton>
+  );
+}
+
+// -------------------------------------------------------
 
 export function SaveBtn(props) {
 
@@ -66,14 +71,17 @@ export function UploadBtn() {
   )
 }
 
-export function BackBtn() {
+//중간사이즈(120x44) 버튼
+export function MiddleBtn({ text,onClickFt }) {
   return (
-    <Button width={120} height={44} hover>
-      이전페이지
+    <Button width={120} height={44} hover onClick={onClickFt}>
+      {text}
     </Button>
   )
 }
 
+// -------------------------------------------------------
+// 색상반전 버튼
 export function ChatBtn() {
   return (<>
     <Button
@@ -87,17 +95,18 @@ export function ChatBtn() {
     </Button>
   </>)
 }
+ 
+// -------------------------------------------------------
+//토글버튼
 
 export function FollowToggleBtn() {
   const [isFollow, setIsFollow] = useState(false)
-
   function onClick() {
     isFollow === false ?
       setIsFollow(true) :
       setIsFollow(false);
     console.log(isFollow)
   }
-
   if (!isFollow) {
     return (
       <Button width={56} height={28}
