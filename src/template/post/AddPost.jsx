153e377d4d070cc8b7e921374dd946054cfb9e4e
minju moon <mmj9908@gmail.com>
2022-07-11 02:04:58 +0900

    :lipstick: Style : 포스팅작성UI
    포스팅을 작성하는 UI를 제작했습니다.

---
+++ src/template/post/AddPost.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {AllWrap,PaddingMain,FormStyle} from '../../style/commonStyle'
+import {SaveNav} from '../../components/navBack/NavBack'
+import ImgUploadBox from '../../components/imgUploadBox/ImgUploadBox'
+import {TitleInput,PetInfoInput,PostMeetTimeInput,PostIntroInput} from '../../components/input/Input'
+export default function AddPost() {
+  return (
+    <AllWrap>
+      <header>
+        <SaveNav/>
+      </header>
+      <PaddingMain>
+        <ImgUploadBox/>
+        <FormStyle>
+          <TitleInput/>
+          <PetInfoInput/>
+          <PostMeetTimeInput/>
+          <PostIntroInput/>
+        </FormStyle>
+      </PaddingMain>
+    </AllWrap>
+  )
+}
