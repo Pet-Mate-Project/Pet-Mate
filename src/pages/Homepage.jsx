b82422911504d3ad2c45a4d82ab65dface568596
sweeeeetpotato <sweet_potato13@naver.com>
2022-07-11 23:45:21 +0900

    :lipstick: Style : 404 페이지 UI 구현
    404 페이지 UI를 위한 NotFoundCompo 컴포넌트 추가, 컴포넌트를 이용하여 404 페이지 UI 구현 완료. 컴포넌트 추가함으로서 FollowCompo의 default 제거 및 관련된 파일에서의 오타와 띄어쓰기 수정

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import {NavSearch} from '../components/navBack/NavBack'
-import {AllWrap} from '../style/commonStyle'
-import FollowCompo from '../components/followCompo/FollowCompo'
-import {AddBtn} from '../components/iconButton/IconButton'
+import { NavSearch } from '../components/navBack/NavBack'
+import { AllWrap } from '../style/commonStyle'
+import { FollowCompo } from '../components/followCompo/FollowCompo'
+import { AddBtn } from '../components/iconButton/IconButton'
 import TabMenu from '../components/tabMenu/TabMenu'
 
-export default function Homepage() {
-  const textBtn ="친구 구하기"
+export default function HomePage() {
+  const textBtn = "친구 구하기"
   const textDefault = "함께 놀 친구들을 찾아보세요! :)"
   return (
     <AllWrap>
@@ -14,9 +14,9 @@ export default function Homepage() {
         <NavSearch text={"친구구해요"} />
       </header>
       <main>
-        <FollowCompo textBtn={textBtn} textDefault={textDefault}/>
-        <AddBtn/>
-        <TabMenu/>
+        <FollowCompo textBtn={textBtn} textDefault={textDefault} />
+        <AddBtn />
+        <TabMenu />
       </main>
     </AllWrap>
   )
