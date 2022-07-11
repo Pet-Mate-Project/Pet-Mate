c3c3600135061e608ea768b301af067b2dd742ad
minju moon <mmj9908@gmail.com>
2022-07-11 23:19:23 +0900

    :sparkles: Feat : 포스팅작성redux셋팅
    포스팅작성기능 구현을 위한 셋팅 및 store생성, reducer생성,index.js셋팅

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client';
+import { Provider } from "react-redux";
+import { store } from './store';
 import App from './App';
 
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
-    <App tab="home" />
-  </BrowserRouter>);
+
+  <Provider store={store}> 
+    <BrowserRouter>
+      <App tab="home" />
+    </BrowserRouter>  
+  </Provider>
+);
