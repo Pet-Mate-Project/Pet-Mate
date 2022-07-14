297c521d824f29a354f1b2ecf232c2782eefbbee
minju moon <mmj9908@gmail.com>
2022-07-14 15:17:03 +0900

    :sparkles: Feat : 게시글등록 및 조건부랜더링
    homepage의 <main>부분을 따로빼서 조건에 따라 다르게 렌더링 시켰습니다.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,37 @@
 import React from 'react'
 import { NavSearch } from '../components/navBack/NavBack'
 import { AllWrap } from '../style/commonStyle'
-import { FollowCompo } from '../components/followCompo/FollowCompo'
-import { AddBtn } from '../components/iconButton/IconButton'
 import TabMenu from '../components/tabMenu/TabMenu'
-import { Link } from 'react-router-dom'
+import { useSelector,useDispatch} from 'react-redux'
+import { useEffect } from 'react'
+import { selectAllPosts,AxiosPetInfo,getPostStatus } from '../reducers/getPetInfoSlice'
+import DefaultFeed from '../template/walkingFeed/DefaultFeed'
+import WalkingFeed from '../template/walkingFeed/WalkingFeed'
 
 export default function HomePage() {
-  const textBtn = "펫 등록하기"
-  const textDefault = "함께 놀 친구들을 찾아보세요! :)"
-  const url = '/post'
-  return (
+
+  const dispatch = useDispatch();
+  const postsStatus = useSelector(getPostStatus);
+  const URL = "https://mandarin.api.weniv.co.kr";
+  const userInfo = JSON.parse(localStorage.getItem("userinfo")).user
+  const accountname = userInfo.accountname;
+  const loginReqPath = `/product/${accountname}/?limit=30`; //내게시글
+  // const loginReqPath = `/product/?limit=100`;  // 전체게시글 100개
+  const posts = useSelector(selectAllPosts).product;
+  useEffect(()=>{
+    if(postsStatus ==='idle'){
+      dispatch(AxiosPetInfo(URL+loginReqPath))
+    }
+  },[dispatch])
+
+  return (          
     <AllWrap>
       <header>
         <NavSearch text={"산책 피드"} />
       </header>
-      <main>
-        <FollowCompo url={url} textBtn={textBtn} textDefault={textDefault} />
-        <Link to={url}>
-          <AddBtn />
-        </Link>
-        <TabMenu />
-      </main>
+      {(postsStatus==='idle'&&posts?.length===0) ?<DefaultFeed/>:<WalkingFeed/> }
+      <TabMenu />
     </AllWrap>
+              
   )
 }
--- src/template/post/HomePost.jsx
+++ src/template/post/HomePost.jsx
@@ -3,26 +3,12 @@ import { ContentTxt, DateTxt, PetImg, PetInfoTxt, PostStyle, TitleTxt, TxtBox } 
 import { UserChat } from '../../components/user/User'
 import { selectAllPosts ,getPostStatus,AxiosPetInfo} from '../../reducers/getPetInfoSlice'
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react'
-
 
 export default function HomePost() {
   const dispatch = useDispatch();
   const posts = useSelector(selectAllPosts).product;
   const postsStatus = useSelector(getPostStatus);
-  console.log(posts);
-  
-  const URL = "https://mandarin.api.weniv.co.kr";
-  const userInfo = JSON.parse(localStorage.getItem("userinfo")).user
-  const accountname = userInfo.accountname;
-  const loginReqPath = `/product/${accountname}/?limit=30`; //내게시글
-  // const loginReqPath = `/product/?limit=100`;  // 전체게시글 100개
-
-  useEffect(()=>{
-    if(postsStatus ==='idle'){
-      dispatch(AxiosPetInfo(URL+loginReqPath))
-    }
-  },[dispatch])
+  console.log("템플릿",posts);
 
   return ( 
     <>
