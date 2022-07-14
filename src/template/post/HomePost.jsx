b7559995609e04c6b52ac5b1b8206fe6ee746e9e
minju moon <mmj9908@gmail.com>
2022-07-15 00:39:25 +0900

    :sparkles: Add : 디폴트이미지를 로고사진으로 변경
    디폴트 이미지로 강아지로고사진 사용했습니다.

--- src/template/post/HomePost.jsx
+++ src/template/post/HomePost.jsx
@@ -8,13 +8,23 @@ import { useDispatch, useSelector } from 'react-redux';
 export default function HomePost() {
   const posts = useSelector(selectAllPosts).product;
   console.log("템플릿",posts);
+  const defaultImg= "https://mandarin.api.weniv.co.kr/1657812669741.png"
+  const marketImg = "http://146.56.183.55:5050/Ellipse.png"
 
+  function imgCheck(post){
+    if  (post.author.image===marketImg){
+      return defaultImg; 
+    }
+    else{
+      return post.author.image;
+    }
+  }
   return ( 
     <ul >
       {posts && posts.map((post)=>{
         return(   
           <PostStyle key={post.id}>
-            <UserChat userName={post.author.username} userId={post.author.accountname} img={post.author.image}/>
+            <UserChat userName={post.author.username} userId={post.author.accountname} img={imgCheck(post)}/>
             <PetImg src={"https://mandarin.api.weniv.co.kr/"+post.itemImage}></PetImg>
             <TxtBox>
               <TitleTxt>{post.itemName}</TitleTxt>
