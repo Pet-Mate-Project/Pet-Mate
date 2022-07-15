ab7f3048f799c6621408623fcb4eedf2c5e2a9e2
sweeeeetpotato <sweet_potato13@naver.com>
2022-07-15 21:58:40 +0900

    :lipstick: Style : 채팅룸 말풍선 컴포넌트 구현
    말풍선의 크기가 가득차지 않도록 width: 55%로 설정, 문장이 음절단위가 아닌 단어단위로 줄바뀜될 수 있도록 함.

---
+++ src/components/speechBubble/SpeechBubble.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { AllWrap } from '../../style/commonStyle'
+import { ProfileIconS } from '../profileIcon/ProfileIcon'
+import { LeftSpeechBubbleWrapper, RightSpeechBubbleWrapper, RightChat, LeftChat, ChatTime } from './speechBubbleStyle'
+
+export default function LeftSpeechBubble() {
+  return (
+    <LeftSpeechBubbleWrapper>
+      <ProfileIconS></ProfileIconS>
+      <LeftChat>
+        아랑이랑 산책하고 싶어서 게시글 시간보고 연락드려요~ 6/28 3시 가능하신가요?
+      </LeftChat>
+      <ChatTime>12:39</ChatTime>
+    </LeftSpeechBubbleWrapper>
+  )
+}
+
+export function RightSpeechBubble() {
+  return (
+    <RightSpeechBubbleWrapper>
+      <ProfileIconS></ProfileIconS>
+      <RightChat>
+        하고있는데요.
+      </RightChat>
+      <ChatTime>12:50</ChatTime>
+    </RightSpeechBubbleWrapper>
+  )
+}
---
+++ src/components/speechBubble/speechBubbleStyle.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const LeftSpeechBubbleWrapper = styled.div`
+  display: flex;
+  padding: 0 16px;
+  margin: 10px 0;
+  gap: 6px;
+`
+
+export const RightSpeechBubbleWrapper = styled(LeftSpeechBubbleWrapper)`
+  flex-direction: row-reverse;
+`
+
+export const LeftChat = styled.span`
+  max-width: 55%;
+  padding: 12px;
+  margin-left: 6px;
+  font-size: 14px;
+  line-height: 17px;
+  border: 1px solid #C4C4C4;
+  border-radius: 0px 10px 10px 10px;
+  word-break: keep-all;
+`
+
+export const RightChat = styled(LeftChat)`
+  border-radius: 10px 0 10px 10px;
+  margin-left: 0;
+  margin-right: 6px;
+`
+
+export const ChatTime = styled.strong`
+  margin-top: auto;
+  font-size: 10px;
+  line-height: 12px;
+  color: #404040;
+`
