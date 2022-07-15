848cbe8d5df0c5fc655d002a1760b9f1c0f85c55
heejin <khj16355@naver.com>
2022-07-16 05:37:30 +0900

    :lipstick: Modify : 산책피드에 글이 없는경우 랜더링되지않음
    산책피드에 글이 없는 경우 랜더링되지 않음

--- src/pages/MyProfilePage.jsx
+++ src/pages/MyProfilePage.jsx
@@ -4,18 +4,28 @@ import { AllWrap } from '../style/commonStyle'
 import MyProfile from '../template/profile/MyProfile'
 import TabMenu from '../components/tabMenu/TabMenu'
 import { PetPost } from '../template/profilePost/PetPost'
-
+import { selectAllPosts } from '../reducers/getPetInfoSlice'
+import { useSelector } from 'react-redux';
 
 function MyProfilePage() {
+
+  const postLength = useSelector(selectAllPosts).product.length;
+
+  console.log('postLength', postLength)
+
   return (
     <AllWrap>
       <NavBack />
       <MyProfile />
-      <PetPost />
-      {/* 작성된 포스팅이 있으면 친구구해요 + 게시글리스트 템플릿 추가 */}
+      {/* 산책피드가 없는 경우 빈화면 */}
+      {
+        postLength === 0 ? " " : <PetPost />
+      }
+      {/* sns 포스트가 없는 경우 추가 */}
       <TabMenu />
     </AllWrap>
   )
+
 }
 
 export default MyProfilePage
