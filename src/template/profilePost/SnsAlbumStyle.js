5f4a7a046ff0b968bfc628e9106f33906d6e8627
heejin <khj16355@naver.com>
2022-07-19 00:45:35 +0900

    :sparkles: Feat : 마이페이지 내게시글 랜더링 앨범형까지 구현완료
    마에페이지 내게시글 앨범형으로 랜더링하기 구현완료

---
+++ src/template/profilePost/SnsAlbumStyle.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const AlbumWrap = styled.div`
+padding: 10px;
+box-sizing: border-box;
+width: 100%;
+display: grid;
+grid-template-columns: repeat(3,1fr);
+gap: 10px;
+`
+
+export const AlbumImage = styled.img`
+width: 100%;
+height: 111px;
+object-fit: cover;
+@media screen and (min-width: 450px){
+height: 130px;
+}
+@media screen and (min-width:600px){
+  height: 181px;
+}
+`
