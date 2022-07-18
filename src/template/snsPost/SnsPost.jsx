1daf80e8fae5669cd5b3ba21136e58a7771ab375
minju moon <mmj9908@gmail.com>
2022-07-18 11:29:28 +0900

    :sparkles: Feat : sns게시글내용렌더링
    sns피드의 게시글들을 렌더링시켰습니다. (아직 팔로우기능이 없기때문에 자신의 글을 불러오도록 했습니다.)

--- src/template/snsPost/SnsPost.jsx
+++ src/template/snsPost/SnsPost.jsx
@@ -1,27 +1,56 @@
 import React from 'react'
 import { UserMore } from '../../components/user/User.jsx'
-import { IconWrap, PostImg, PostText, WrapperArticle, WrapSection, IconImg, DateText, MoreIcon } from './snsPostStyle'
+import { IconWrap, PostImg, PostText, WrapperArticle, WrapSection, IconImg, DateText } from './snsPostStyle'
 import image from '../../assets/basic-profile.svg'
 import heartIcon from '../../assets/icon-heart.svg'
 import messageIcon from '../../assets/icon-message.svg'
+import { useSelector } from 'react-redux';
+import { selectAllSnsPosts } from '../../reducers/getPostSlice.js'
 
 function SnsPost() {
-  return (
-    <article>
-      <WrapperArticle>
-        <UserMore />
-        <WrapSection>
-          <PostText>우리 아랑이가 드디어 코딩을 할 줄 알아요..!! 우리 애기 천재💗🐿️</PostText>
-          <PostImg src={image} />
-          <IconWrap>
-            <IconImg src={heartIcon} />38
-            <IconImg src={messageIcon} />55
-          </IconWrap>
-          <DateText>2022년 07월 01일</DateText>
-        </WrapSection>
-      </WrapperArticle>
+  const  snsPosts = useSelector(selectAllSnsPosts).post;
+  console.log(snsPosts);
+  const defaultImg = "https://mandarin.api.weniv.co.kr/1657812669741.png";
+  const marketImg = "http://146.56.183.55:5050/Ellipse.png";
 
-    </article>
+  function imgCheck(post) {
+    if (post.author.image === marketImg) {
+      return defaultImg;
+    }
+    else {
+      return "https://mandarin.api.weniv.co.kr/" + post.author.image;
+    }
+  }
+
+  return (
+    <ul>
+      { snsPosts && snsPosts.map((post)=>{
+        let images = post.image
+        images = images.split(",")
+        console.log(images);
+        return(
+          <li key={post.id} style={{margin:"30px 0"}}>
+            <UserMore userName={post.author.username} userId={post.author.accountname} img={imgCheck(post)}/>
+            <WrapSection>
+              <PostText>{post.content}</PostText>
+              { images.map((image)=>{
+                console.log("https://mandarin.api.weniv.co.kr/"+image);
+                return(
+                  <>
+                    <PostImg src={"https://mandarin.api.weniv.co.kr/"+image} />
+                  </>
+                )
+              })}
+              <IconWrap>
+                <IconImg src={heartIcon} />38
+                <IconImg src={messageIcon} />55
+              </IconWrap>
+              <DateText>{post.updatedAt.substring(0, 10)}</DateText>
+            </WrapSection>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
