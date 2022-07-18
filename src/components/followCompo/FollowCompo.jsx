58d6f36aae3f59b67af11eb08dc3307418702b3a
minju moon <83548784+mandelina@users.noreply.github.com>
2022-07-18 17:49:05 +0900

    Revert ":sparkles: Feat : url로 직접 접근시 조건부렌더링"

--- src/components/followCompo/FollowCompo.jsx
+++ src/components/followCompo/FollowCompo.jsx
@@ -3,7 +3,8 @@ import { FollowCompoWrapper, DefaultImg, DefaultTxt } from './followCompoStyle'
 import grayLogo from '../../assets/gray-logo.svg'
 import logo404 from '../../assets/404-logo.svg'
 import { MiddleBtn } from '../../components/button/Button'
-import { Link,useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 export function FollowCompo(props) {
   return (
@@ -20,14 +21,13 @@ export function FollowCompo(props) {
 }
 
 export function NotFoundCompo() {
-  const navigate = useNavigate();
   return (
     <FollowCompoWrapper>
       <img src={logo404} />
       <DefaultTxt>
         페이지를 찾을 수 없습니다. :(
       </DefaultTxt>
-      <MiddleBtn textBtn={'이전 페이지'} onClickFt={()=>navigate(-1)}/>
+      <MiddleBtn textBtn={'이전 페이지'} />
     </FollowCompoWrapper>
   )
 }
