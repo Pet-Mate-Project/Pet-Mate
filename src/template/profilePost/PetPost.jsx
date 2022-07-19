ccd35446628a373b7d655d4c92ad7636b644e8ff
minju moon <mmj9908@gmail.com>
2022-07-19 23:39:07 +0900

    :sparkles: Feat : 내 프로필 sns게시글 delete 요청
    Feat : 내 프로필 sns게시글 삭제성공 , 삭제 시 모달 닫힘기능 , 삭제 후 삭제된 게시글이 업데이트

--- src/template/profilePost/PetPost.jsx
+++ src/template/profilePost/PetPost.jsx
@@ -1,11 +1,13 @@
 import React,{useState} from 'react'
 import { AnimalBox } from '../../components/animalBox/AnimalBox'
 import { selectAllPosts } from '../../reducers/getPetInfoSlice'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux'
 import { MiniPostTitle, MiniPostWrap, SectionAllWrap } from './petPostStyle'
 import Modal from '../../components/postModal/PostModal';
+import { deleteActions,getDeleteStatus } from '../../reducers/deletePostSlice'
 
 export function PetPost() {
+  const dispatch = useDispatch();
   const posts = useSelector(selectAllPosts).product;
   const list = {'삭제':'','수정':'/','산책피드로 가기':'/homepage'};
   const alertTxt=['삭제하시겠어요?','삭제'];
@@ -14,23 +16,25 @@ export function PetPost() {
   const closeModal = () => {
     setModal(false)
   }
-
-  const toggleModal = () => {
+  //id값을 store에 저장하는 오는 함수
+  const handleId = (petId)=>{
+    dispatch(deleteActions.checkType("product"));
+    dispatch(deleteActions.deleteId(petId));
     setModal(modal => !modal)
-    console.log(modal);
   }
-  
+
   return (
     <SectionAllWrap>
       <MiniPostTitle>산책가까?</MiniPostTitle>
       {
-        modal=== true && <Modal list={list} alertTxt={alertTxt} closeModal={closeModal} />}
+        modal=== true && <Modal list={list} alertTxt={alertTxt} closeModal={closeModal} setModal={setModal} />}
       <MiniPostWrap>
         {posts && posts.map((post) => {
-          return (
+          return (<>
             <AnimalBox key={post.id}
               src={`https://mandarin.api.weniv.co.kr/${post.itemImage}`}
-              title={post.itemName} time={post.updatedAt.substring(0, 10)} onClick={toggleModal} />
+              title={post.itemName} time={post.updatedAt.substring(0, 10)} onClick={() => handleId(post.id)} />
+          </>
           )
         }
         )}
