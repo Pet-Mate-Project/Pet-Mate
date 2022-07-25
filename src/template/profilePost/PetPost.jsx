2cf7e4a8a51fb7c24e225e1ddb11ac766419da0d
sweeeeetpotato <sweet_potato13@naver.com>
2022-07-26 03:46:21 +0900

    :sparkles: Add : 프로필에서 산책게시글 클릭시 url이동하도록 라우터 설정
    myprofile에서 산책게시글 클릭시 모달이 뜨는데 모달3번째 리스트에 상세페이지로 이동할 수 있도록 구현, yourprofile에서는 산책게시글 클릭시 바로 상세페이지로 이동할 수 있도록 설정, 상세페이지안에서의 이미지 사이즈 제한

--- src/template/profilePost/PetPost.jsx
+++ src/template/profilePost/PetPost.jsx
@@ -1,45 +1,65 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { AnimalBox } from '../../components/animalBox/AnimalBox'
 import { selectAllPosts } from '../../reducers/getPetInfoSlice'
 import { useSelector, useDispatch } from 'react-redux'
 import { MiniPostTitle, MiniPostWrap, SectionAllWrap } from './petPostStyle'
 import Modal from '../../components/postModal/PostModal';
 import { deleteActions } from '../../reducers/deletePostSlice'
-import {useLocation} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import { SelectId } from '../../reducers/deletePostSlice'
+import { Link } from 'react-router-dom'
 
 export function PetPost() {
   const location = useLocation();
   const name = location.state?.userId;
   const dispatch = useDispatch();
   const posts = useSelector(selectAllPosts).product;
-  const list = {'삭제':'','수정':'/postmodify','산책피드로 가기':'/homepage'};
-  const alertTxt=['삭제하시겠어요?','삭제'];
+  const list = { '삭제': '', '수정': '/postmodify', '상세페이지로 가기': '' };
+  const alertTxt = ['삭제하시겠어요?', '삭제'];
   const [modal, setModal] = useState(false);
+  const postId = useSelector(SelectId);
 
   const closeModal = () => {
     setModal(false)
   }
+
   //id값을 store에 저장하는 오는 함수
-  const handleId = (petId)=>{
+  const handleId = (petId) => {
     dispatch(deleteActions.checkType("product"));
     dispatch(deleteActions.selectId(petId));
-    setModal(modal => !modal)
+    setModal(modal => !modal);
   }
 
   return (
     <SectionAllWrap>
       <MiniPostTitle>산책가까?</MiniPostTitle>
       {
-        (modal=== true )&&(name ===undefined)&&<Modal list={list} alertTxt={alertTxt} closeModal={closeModal} setModal={setModal} />}
+        (modal === true) && (name === undefined) && (list['상세페이지로 가기'] = `/walkingpostdetail/${postId}`) && <Modal list={list} alertTxt={alertTxt} closeModal={closeModal} setModal={setModal} />
+      }
+      {
+        (modal === true) && (name !== undefined)
+      }
       <MiniPostWrap>
-        {posts && posts.map((post) => {
-          return (
-            <AnimalBox key={post.id}
-              src={`https://mandarin.api.weniv.co.kr/${post.itemImage}`}
-              title={post.itemName} time={post.updatedAt.substring(0, 10)} onClick={() => handleId(post.id)} />
-          )
+        {
+          posts && posts.map((post) => {
+            return (
+              name === undefined
+                ?
+                <AnimalBox key={post.id}
+                  src={`https://mandarin.api.weniv.co.kr/${post.itemImage}`}
+                  title={post.itemName} time={post.updatedAt.substring(0, 10)} onClick={() => handleId(post.id)} />
+                :
+                <Link to={'/walkingpostdetail/' + post.id}>
+                  <AnimalBox key={post.id}
+                    src={`https://mandarin.api.weniv.co.kr/${post.itemImage}`}
+                    title={post.itemName} time={post.updatedAt.substring(0, 10)} onClick={() => handleId(post.id)} />
+                </Link>
+            )
+          })
         }
-        )}
+        {
+
+        }
       </MiniPostWrap>
     </SectionAllWrap>
   )
