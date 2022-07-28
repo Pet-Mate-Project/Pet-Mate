8a4a50cf7043bf6f48da7639b4dd35f6505c2d6a
sweeeeetpotato <sweet_potato13@naver.com>
2022-07-28 15:32:28 +0900

    :lipstick: Style : splash화면 애니메이션 추가
    splash화면과 로그인 및 회원가입 버튼이 뜨는 Main화면에 키프레임으로 애니메이션기능 추가

--- src/template/main/mainStyle.js
+++ src/template/main/mainStyle.js
@@ -1,22 +1,82 @@
-import styled from 'styled-components';
-import petLogo from '../../assets/pet-logo.svg'
-import fullPetLogo from '../../assets/full-logo.svg'
+import styled, { keyframes } from 'styled-components';
+import BeforeLogo from '../../assets/before-logo.svg'
+import AfterLogo from '../../assets/after-logo.svg'
+import MainLogo from '../../assets/main-logo.svg'
+import { PaddingMain, FormStyle } from '../../style/commonStyle';
 
-export const LogoStyle = styled.div`
-  height: 240px;
-  width: 240px;
-  background: url(${petLogo}) no-repeat;
-  margin: 191px auto 113px;
+const logoFadeIn = keyframes`
+  0% {
+    opacity: 0;
+    background: url(${BeforeLogo}) no-repeat;
+    background-size: 300px 300px;
+    }
+  30% {
+    opacity: 0.4;
+    background: url(${BeforeLogo}) no-repeat;
+    background-size: 300px 300px;
+    }
+  55% {
+    opacity: 0.7;
+    background: url(${AfterLogo}) no-repeat;
+    background-size: 300px 300px;
+    }
+  100% {
+    opacity: 1;
+    background: url(${AfterLogo}) no-repeat;
+    background-size: 300px 300px;
+    
+  }
 `
-export const Gap = styled.div`
-margin-bottom : 24px;
+
+const logoTextFadeOut = keyframes`
+  from {
+    background: url(${AfterLogo}) no-repeat;
+    background-size: 300px 300px;
+  }
+  to {
+    background: url(${MainLogo}) no-repeat;
+    background-size: 300px 300px;
+  }
 `
-export const FullLogoStyle = styled.div`
-  height: 270px;
-  width: 270px;
-  background: url(${fullPetLogo}) no-repeat;
+
+const buttonSlideUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+`
+
+export const Wrapper = styled(PaddingMain)`
+  width: 100%;
   position:absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+  box-sizing: border-box;
+`
+
+export const Gap = styled(FormStyle)`
+  margin: 0;
+  opacity: 0;
+  gap: 24px;
+  animation: ${buttonSlideUp} linear 0.5s forwards;
+`
+
+export const FullLogoStyle = styled.div`
+  height: 300px;
+  width: 300px;
+  margin: 0 auto 112px;
+  animation: ${logoFadeIn} linear 1s forwards;
+
+`
+
+export const LogoStyle = styled.div`
+  height: 300px;
+  width: 300px;
+  margin: 0 auto;
+  animation: ${logoTextFadeOut} linear 0.5s forwards;
 `
