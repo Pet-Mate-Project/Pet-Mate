a5313e8b63167d305007387f87681d854f8c625e
minju moon <mmj9908@gmail.com>
2022-07-31 23:27:08 +0900

    :art: Modify : components 코드정리
    components 코드정리

--- src/components/profile/profileStyle.js
+++ src/components/profile/profileStyle.js
@@ -2,30 +2,30 @@ import styled from 'styled-components'
 import imgUpload from "../../assets/upload-file.svg"
 
 export const Wrapper = styled.div`
-width: 120px;
-height: 120px;
-border-radius: 50%;
-position: relative;
-margin: 0 auto;
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  position: relative;
+  margin: 0 auto;
 `
 export const FileUploader = styled.label`
-background: url(${(imgUpload)});
-background-repeat: no-repeat;
-background-size: 36px;
-position: absolute;
-width: 36px;
-height: 36px;
-bottom: 0;
-right : 0;
+  background: url(${(imgUpload)});
+  background-repeat: no-repeat;
+  background-size: 36px;
+  position: absolute;
+  width: 36px;
+  height: 36px;
+  bottom: 0;
+  right : 0;
 `
 
 export const ProfileImg = styled.img`
-width: 120px;
-height: 120px;
-border-radius: 50%;
-object-fit: cover;
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  object-fit: cover;
 `
 
 export const FileInput = styled.input`
-display: none;
+  display: none;
 `
