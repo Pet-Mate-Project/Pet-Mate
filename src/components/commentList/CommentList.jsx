eaa37395400f7261d1f195637958b89fa7b5301e
sweeeeetpotato <sweet_potato13@naver.com>
2022-07-31 21:46:02 +0900

    :sparkles: Add : 댓글클릭시 유저프로필 페이지로 이동하도록 라우팅 설정
    SNS게시글 상세페이지에 존재하는 댓글의 프로필정보를 클릭하면 해당 사용자의 프로필페이지로 이동할 수 있도록 구현

--- src/components/commentList/CommentList.jsx
+++ src/components/commentList/CommentList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { ProfileIconS } from '../profileIcon/ProfileIcon'
 import moment from 'moment';
 import "moment/locale/ko";
@@ -6,18 +7,23 @@ import dot from '../../assets/icon-more-vertical.svg'
 import { CommentWrpper, Name, WritingTime, Content, UserMore } from './commentListStlye';
 
 export default function CommentList({ content, time, author, img, onClick }) {
+  const accountname = JSON.parse(localStorage.getItem("accountname"));
+  let linkName = '';
+  accountname === author ? linkName = '/profilepage' : linkName = '/userprofile';
 
   return (
     <li style={{ margin: "20px 16px" }}>
-      <CommentWrpper >
-        <ProfileIconS img={img} alt={"프로필 사진"} />
-        <Name>{author}</Name>
-        <WritingTime>· {moment(time).fromNow()}</WritingTime>
+      <CommentWrpper>
+        <Link to={linkName} state={{ userId: author }} style={{ display: "flex" }}>
+          <ProfileIconS img={img} alt={"프로필 사진"} />
+          <Name>{author}</Name>
+          <WritingTime>· {moment(time).fromNow()}</WritingTime>
+        </Link>
         <button style={{ marginLeft: "auto" }}>
           <UserMore src={dot} onClick={onClick} alt="댓글설정 버튼" />
         </button>
       </CommentWrpper>
       <Content>{content}</Content>
-    </li>
+    </li >
   )
 }
